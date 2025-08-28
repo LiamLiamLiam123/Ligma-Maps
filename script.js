d45208901869c41907aaafd84dfca4d67d2698c9
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2025-08-29 09:57:31 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-mapboxgl.accessToken = "pk.eyJ1IjoiY29kZW15aG9iYnkiLCJhIjoiY2tqbW1jNmxkMHg4bzJzbWpmOXFpenY2byJ9.NshhLJgIl46JtWT6ZhKEDw"
+mapboxgl.accessToken = "pk.eyJ1IjoiY29kZW15aG9iYnkiLCJhIjoiY2tqbW1jNmxkMHg4bzJzbWpmOXFpenY2byJ9.NshhLJgIl46JtWT6ZhKEDw";
 
 navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
   enableHighAccuracy: true
-})
+});
 
-function successLocation(position){
-    setUpMap([position.coords.longitude, position.coords.latitude])
+function successLocation(position) {
+    setUpMap([position.coords.longitude, position.coords.latitude]);
 }
 
-function errorLocation(){
-    setUpMap([-2.28, 41.45])
+function errorLocation() {
+    setUpMap([-2.28, 41.45]);
 }
 
-function setUpMap(center){
+function setUpMap(center) {
     var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: center,
-    zoom: 15
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 15
     });
 
     map.addControl(new mapboxgl.NavigationControl());
 
+    // Add directions with metric units
     map.addControl(
         new MapboxDirections({
-        accessToken: mapboxgl.accessToken
+            accessToken: mapboxgl.accessToken,
+            unit: 'metric'
         }),
         'top-left'
-        );
+    );
 }
