29d8099c7efc1822d8b52ab2d95314dff14ef0c2
LiamLiamLiam123 <163282026+LiamLiamLiam123@users.noreply.github.com>
2025-08-28 20:44:21 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken = "pk.eyJ1IjoiY29kZW15aG9iYnkiLCJhIjoiY2tqbW1jNmxkMHg4bzJzbWpmOXFpenY2byJ9.NshhLJgIl46JtWT6ZhKEDw"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position){
+    setUpMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation(){
+    setUpMap([-2.28, 41.45])
+}
+
+function setUpMap(center){
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: center,
+    zoom: 15
+    });
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+    map.addControl(
+        new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+        }),
+        'top-left'
+        );
+}
